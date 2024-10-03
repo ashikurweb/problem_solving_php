5d9441f3839bfc8848885b9a00ca7f3bb4e0566f
Ashikur Rahman <ashikursec>
2024-10-04 01:07:46 +0600

    grade_mark

---
+++ grade_mark/index.php
@@ -0,0 +1,52 @@
+<?php 
+
+// $marks = 80;
+
+
+// if ( $marks >= 80 ) {
+//     $result = 'A+';
+// } else if ( $marks >= 70 ) {
+//     $result = 'A';
+// } else if ( $marks >= 60 ) {
+//     $result = 'A-';
+// } else if ( $marks >= 50 ) {
+//     $result = 'B';
+// } else if ( $marks >= 40 ) {
+//     $result = 'C';
+// } else {
+//     $result = "Fail";
+// }
+
+// echo $result;
+
+
+//  Using Switch
+
+$marks = 60;
+
+switch( $marks ) {
+    case '80':
+        $result = 'A+';
+    break;
+
+    case '70':
+        $result = 'A';
+    break;
+
+    case '60':
+        $result = 'A-';
+    break;
+
+    case '50':
+        $result = 'B';
+    break;
+
+    case '40':
+        $result = 'C';
+    break;
+
+    default:
+        $result = 'Fail';
+}
+
+echo $result;
